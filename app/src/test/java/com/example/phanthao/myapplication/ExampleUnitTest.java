f88272da3e44694ed10cce2b884773ead15ef4f2
phanthaoIT <ptthao.itus@gmail.com>
2018-11-09 09:04:26 +0700

    update script

--- app/src/test/java/com/example/phanthao/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/phanthao/myapplication/ExampleUnitTest.java
@@ -1,9 +1,13 @@
 package com.example.phanthao.myapplication;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -14,10 +18,12 @@ import io.appium.java_client.android.AndroidTouchAction;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
 
+import static org.junit.Assert.assertEquals;
+
 
 public class ExampleUnitTest {
     AndroidDriver driver;
-    @Before
+    @BeforeTest
     public void init() throws MalformedURLException {
         System.out.println("Start Test");
         DesiredCapabilities cap = new DesiredCapabilities();
@@ -31,41 +37,65 @@ public class ExampleUnitTest {
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"),cap);
         driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
     }
-    @Test
-    public void runTest(){
-        System.out.println("Run Test");
-        AndroidTouchAction touchAction = new AndroidTouchAction(driver);
+    @Test(dataProvider = "DataTest")
+    public void testRun(String sDiemDi, String sDiemDen, String ngayDi, String ngayVe) {
+        System.out.println("testrun");
 
-        MobileElement btnFilter = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.support.v4.view.ViewPager/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.ImageView");
-        if(btnFilter.isDisplayed()) {
-            touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(btnFilter))).perform();
+        AndroidTouchAction touchAction = new AndroidTouchAction(driver);
+//        Bấm vào nút thêm mới
+        MobileElement btnAdd = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.support.v4.view.ViewPager/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.ImageView");
+        if(btnAdd.isDisplayed()) {
+            touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(btnAdd))).perform();
             MobileElement txtDiemDi = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.EditText");
             touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(txtDiemDi))).perform();
             txtDiemDi.clear();
-            txtDiemDi.setValue("Ha Noi");
+            txtDiemDi.setValue(sDiemDi);
 
             MobileElement txtDiemDen = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.EditText");
             touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(txtDiemDen))).perform();
             txtDiemDen.clear();
-            txtDiemDen.setValue("TP HCM");
+            txtDiemDen.setValue(sDiemDen);
 
-            driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[10]/android.view.ViewGroup[2]/android.view.View").click();
+            MobileElement txtNgayDi = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[6]/android.widget.TextView");
+            touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(txtNgayDi))).perform();
+            txtNgayDi.clear();
+            txtNgayDi.setValue(ngayDi);
 
+            MobileElement txtNgayVe = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[7]/android.widget.TextView");
+            touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(txtNgayVe))).perform();
+            txtNgayVe.clear();
+            txtNgayVe.setValue(ngayVe);
 
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            MobileElement btnok = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup[10]/android.view.ViewGroup[2]/android.view.View");
+            touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(btnok))).perform();
 
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    @After
+
+    @DataProvider(name = "DataTest")
+    public static Object[][] datas() {
+        return new Object[][] {
+                {"","","",""}
+        };
+    }
+
+    @AfterTest
     public void after(){
         System.out.println("End Test");
         driver.quit();
